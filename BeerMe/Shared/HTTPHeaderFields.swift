df261de431aaa3e1d378a2e2ca15c0b180e4192b
Ian Koller <ikoller47@gmail.com>
2019-06-10 22:31:14 -0500

    Added shared HTTP Headers to struct

---
+++ BeerMe/Shared/HTTPHeaderFields.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPHeaderFields.swift
+//  BeerMe
+//
+//  Created by Ian Koller on 5/28/19.
+//  Copyright © 2019 Sapphire. All rights reserved.
+//
+
+import Foundation
+
+struct HTTPHeaderFields {
+    static let authorization = "Authorization"
+}
