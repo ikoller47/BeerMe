99ec513b857c9e05ece082b720932dec7fe36246
Ian Koller <ikoller47@gmail.com>
2019-06-17 16:14:42 -0500

    Added base BRViewModel

---
+++ BeerMe/Components/Base/BRViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  BRViewModel.swift
+//  BeerMe
+//
+//  Created by Ian Koller on 6/16/19.
+//  Copyright © 2019 Sapphire. All rights reserved.
+//
+
+import Foundation
+
+protocol BRViewModelProtocol {
+
+}
+
+class BRViewModel: BRViewModelProtocol {
+    
+}
